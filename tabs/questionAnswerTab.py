a67afb09d6b7a2810e1929cc74a70795d9244d60
Nicolas Imke <Imke.Nicolas@gmail.com>
2022-11-15 14:34:37 +0100

    questionAnswer test

--- tabs/questionAnswerTab.py
+++ tabs/questionAnswerTab.py
@@ -1,10 +1,22 @@
 from gradio_graph import gradio as gr
+import requests
+
+
+def test(link, query):
+    req = link + query + '?&lang=en'
+    response = requests.post(req)
+    print(response)
+    return response
 
 
 with gr.Blocks() as questionAnswerTab:
     with gr.Row():
-        gr.TextArea(label='Question')
-        gr.TextArea(label='Answer', interactive=False)
+        question = gr.TextArea(label='Question', value='Where is the birthplace of Angela Merkel')
+        result = gr.JSON(label='Answer', interactive=False)
     with gr.Row():
-        gr.Text(label="URL")
-        gr.Button()
+        link  =gr.Text(label="URL", value='http://localhost:8080/qa?query=')
+        qaRun = gr.Button()
+
+    qaRun.click(fn=test, inputs=[link,question], outputs=result)
+
+        
