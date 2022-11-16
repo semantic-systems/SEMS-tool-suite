adccad8db614ab0dee160b68ea09e4527361dec7
Nicolas Imke <Imke.Nicolas@gmail.com>
2022-11-16 17:18:08 +0100

    twitter test

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,30 +1,29 @@
-import tweepy, re, os
-from dotenv import load_dotenv
-
-load_dotenv()
+# import tweepy, re, os
 
 
-__auth = tweepy.OAuth1UserHandler(os.getenv('api_key'), os.getenv('api_secret'), os.getenv('access_token'), os.getenv('access_token_secret'))
-api = tweepy.API(__auth)
+
+# __auth = tweepy.OAuth1UserHandler(os.getenv('api_key'), os.getenv('api_secret'), os.getenv('access_token'), os.getenv('access_token_secret'))
+# api = tweepy.API(__auth)
 
 class TwitterFunctions:
     
-    def getTweet(self, query : str) -> str:
-        '''Searches for Tweets with the given query and returns one randomly'''
-        if query == '':
-            return ''
-        tweets = api.search_tweets(q=query, count=100)
-        tweet = self.clean_tweet(tweets[0].text)
-        return tweet
+     def getTweet(self, query : str) -> str:
+        return "test"
+    #     '''Searches for Tweets with the given query and returns one randomly'''
+    #     if query == '':
+    #         return ''
+    #     tweets = api.search_tweets(q=query, count=100)
+    #     tweet = self.clean_tweet(tweets[0].text)
+    #     return tweet
 
     # stolen from previous code ;)
-    def clean_tweet(self, tweet : str):
-        '''
-        Utility function to clean tweet text by removing links, special characters
-        using simple regex statements.
-        '''
-        if tweet.startswith("RT @") :
-            tweet = tweet.replace("RT ", "")
-        tweet = re.sub("@[A-Za-z0-9_]+","", tweet)
-        tweet = re.sub("#[A-Za-z0-9_]+","", tweet)
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+    # def clean_tweet(self, tweet : str):
+    #     '''
+    #     Utility function to clean tweet text by removing links, special characters
+    #     using simple regex statements.
+    #     '''
+    #     if tweet.startswith("RT @") :
+    #         tweet = tweet.replace("RT ", "")
+    #     tweet = re.sub("@[A-Za-z0-9_]+","", tweet)
+    #     tweet = re.sub("#[A-Za-z0-9_]+","", tweet)
+    #     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
