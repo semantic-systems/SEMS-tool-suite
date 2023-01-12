076f08a247ca8b6740ff4cbea66312a83ccbfd22
sch-28 <jansch2803@googlemail.com>
2023-01-12 21:54:30 +0100

    refactor: changed main to improve development w/ gradio

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import gradio as gr
 from tabs import *
 
 
-def main() -> None:
-    '''main gradio function that defines all tabs'''
-    with gr.Blocks(css="#row {height: 100%}") as demo:
-        with gr.Tab('KG Connect & Visualization'):
-            kgVisualizationTab.render()
-        #with gr.Tab('Knowledge Extraction'):
-        #    knowledgeExtractionTab.render()
-        with gr.Tab('Event Extraction'):
-            eventExtractionTab.render()
-        #with gr.Tab('Maximum Damage'):
-        #    maximumDamageTab.render()
-        with gr.Tab('Question Answering'):
-            questionAnswerTab.render()
-    demo.launch(server_name="0.0.0.0")
+'''main gradio function that defines all tabs'''
+with gr.Blocks(css="#row {height: 100%}") as demo:
+    with gr.Tab('KG Connect & Visualization'):
+        kgVisualizationTab.render()
+    #with gr.Tab('Knowledge Extraction'):
+    #    knowledgeExtractionTab.render()
+    with gr.Tab('Event Extraction'):
+        eventExtractionTab.render()
+    #with gr.Tab('Maximum Damage'):
+    #    maximumDamageTab.render()
+    with gr.Tab('Question Answering'):
+        questionAnswerTab.render()
+demo.launch(server_name="0.0.0.0")
 
 
 if __name__ == "__main__":
-    main()
+    demo.launch()
