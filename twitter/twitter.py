fde7a4efeb721ac6a352bce423e186823e83d32e
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-02-16 00:51:47 +0800

    :sparkles: remove unrelated folder

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -5,10 +5,10 @@ from os.path import join, dirname
 dotenv_path = join(dirname(__file__), 'twitter.env')
 load_dotenv(dotenv_path)
 
-api_key = os.environ.get("api_key")
-api_secret = os.environ.get("api_secret")
-access_token = os.environ.get("access_token")
-access_token_secret = os.environ.get("access_token_secret")
+api_key = "7FAdW0Idu11ys3QVy5JIyI7M4" #os.environ.get("api_key")
+api_secret = "jmPbdUiP9N5RVTrJE7c4iM8H95srVgBVP3oT0VCPxw13kBCJjS" #os.environ.get("api_secret")
+access_token = "1580905286377807872-VufqIF3gEk0lkZyM4ZiHNBWwLZADxk" # os.environ.get("access_token")
+access_token_secret = "LMdhVx3gKSDB6GvvC3WO4aFojATyRfr4MS368AbxNgMop" # os.environ.get("access_token_secret")
 
 
 __auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
