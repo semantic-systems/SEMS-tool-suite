47d066ef63ca9e5ac90aa4d204fa7afe99b0e541
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-06-14 20:17:04 +0200

    :bug: fix interface

--- tabs/UniversalEventDetectionTab.py
+++ tabs/UniversalEventDetectionTab.py
@@ -7,16 +7,22 @@ def utc(message, scheme):
     try:
         url = 'https://sc.hitec.skynet.coypu.org'
         headers = {'Content-Type': 'application/json'}
+        scheme = [element.strip() for element in scheme.split(',')]
         output = requests.post(url, json={'message': message, "key": "GBGBE3THWLF9FB2U", "scheme": scheme}, headers=headers).json()
         return output
     except Exception as e:
         return e
 
 
-examples=["\"One-in-100-year flood event\" devastates Western Australia",
+text_examples=["\"One-in-100-year flood event\" devastates Western Australia",
           "118th United States Congress convenes; House of Representatives adjourns without electing Speaker for first time in 100 years.",
           "UK Treasury considering plans for digital pound, economic secretary says.",
           "Troops freed by Mali return to Ivory Coast."]
+
+scheme_examples=["Environmental event, Societal event, Governmental event",
+          "Exchange, Buy in, Payment, Sell out, Expressing publicly, Institutionalization, Bankruptcy",
+          "Feeling happy, Feeling sad, Feeling angry, Feeling melancholic, Feeling calm, Feeling like a child",
+          "Exposition, Rising action, Climax, Falling action, Resolution"]
 
 
 with gr.Blocks() as UniversalEventDetectorTab:
@@ -28,14 +34,19 @@ with gr.Blocks() as UniversalEventDetectorTab:
         # Inputs
         with gr.Column():
             input_box = gr.TextArea(label='Input text')    
-            with gr.Accordion("Examples", open=False):
-                gr.Examples(examples, inputs=input_box, label='')
-            with gr.Accordion("Get examples from Gdelt", open=False, visible=True):
+            scheme_box = gr.TextArea(label='Scheme')
+            with gr.Accordion("Text Examples", open=False):
+                gr.Examples(text_examples, inputs=input_box, label='')
+            with gr.Accordion("Scheme Examples", open=False):
+                gr.Examples(scheme_examples, inputs=scheme_box, label='')
+            with gr.Accordion("Get real news from Gdelt", open=False, visible=True):
                 twitter_input_box = gr.Text(label='Query for Feeds')
                 getFeedButton = gr.Button("Query Feed")
             with gr.Row():
                 with gr.Column():
                     delete_input_button = gr.Button("Delete", elem_id='delete')
+                with gr.Column():
+                    delete_scheme_button = gr.Button("Delete", elem_id='delete')
                 with gr.Column():
                     runEEButton = gr.Button("Run UED", variant='primary')
 
@@ -46,4 +57,5 @@ with gr.Blocks() as UniversalEventDetectorTab:
     # Functions
     getFeedButton.click(fn=GdeltFunctions.get_feed, inputs=twitter_input_box, outputs=input_box)
     delete_input_button.click(fn=lambda:"", inputs=[], outputs=input_box)
-    runEEButton.click(fn=utc, inputs=input_box, outputs=output_box_label)
+    delete_scheme_button.click(fn=lambda:"", inputs=[], outputs=scheme_box)
+    runEEButton.click(fn=utc, inputs=[input_box, scheme_box], outputs=output_box_label)
