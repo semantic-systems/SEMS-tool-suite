aa700789aad84adba62d81ac51c78c1e3fe34ac7
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-10-19 12:00:12 +0200

    :sparkle: remove reformulation of the question due to its long inference time

--- tabs/questionAnswerTab.py
+++ tabs/questionAnswerTab.py
@@ -14,26 +14,21 @@ description = """- Vicuna 1.5-based QA system """
 
 def qa(question):
     try:
-        print(question)
         url = 'https://turbo.skynet.coypu.org/'
+        # request = requests.post(url, json={"messages": [{"role": "user",
+        #        "content": f"You are a question answering system expert. Please reformulate the following sentence into a reasonable question for a LLM. /n/n {question} /n/n "}],
+        #                         "temperature": 0.1,
+        #                         "key": "M7ZQL9ELMSDXXE86"
+        # }).json()
+        # reforumated_question = request[0].get('choices')[0].get("message").get("content")
         request = requests.post(url, json={"messages": [{"role": "user",
-               "content": f"You are a question answering system expert. Please reformulate the following sentence into a reasonable question for a LLM. /n/n {question} /n/n "}],
-                                "temperature": 0.1,
-                                "key": "M7ZQL9ELMSDXXE86"
-        }).json()
-        print("request", request)
-
-        reforumated_question = request[0].get('choices')[0].get("message").get("content")
-        print("reforumated_question", reforumated_question)
-        request = requests.post(url, json={"messages": [{"role": "user",
-                                                "content": reforumated_question}],
+                                                "content": question}],
                                 "temperature": 0.1,
                                 "max_new_tokens": 120,
                                  "key": "M7ZQL9ELMSDXXE86"}).json()
-        print("answer", request[0].get('choices')[0].get("message").get("content"))
-        return reforumated_question, request[0].get('choices')[0].get("message").get("content")
+        return request[0].get('choices')[0].get("message").get("content")
     except Exception as e:
-        return e, e
+        return e
 
 
 with gr.Blocks() as questionAnswerTab:
@@ -45,10 +40,10 @@ with gr.Blocks() as questionAnswerTab:
             gr.Examples(examples, inputs=question_box, label='Example questions')
             qaRun = gr.Button(variant='primary')
         with gr.Column():
+            # reformulated_question_box = gr.JSON(label='Reformulated question', interactive=False)
             answer_box = gr.JSON(label='Answer', interactive=False)
-            reformulated_question_box = gr.JSON(label='Reformulated question', interactive=False)
 
-    qaRun.click(fn=qa, inputs=question_box, outputs=[reformulated_question_box, answer_box])
+    qaRun.click(fn=qa, inputs=question_box, outputs=[answer_box])
 
 
 # if __name__ == "__main__":
