9d28a4b0de1290265bb0b93b5187483711d08a3d
Rana Abdullah <rana.abdullah@gmail.com>
2024-03-13 12:24:53 +0100

    Added Impressum and Datenschutz links in the footer

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from tabs import kgVisualizationTab, eventExtractionTab, questionAnswerTab, Univ
 
 title = "Coypu MoD"
 
+css = """
+#row {height: 100%} 
+.gradio-container {background-color: #E8E8DC}
+.custom-footer {    
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 16px;
+    font-weight: 600;
+    --tw-text-opacity: 1;
+    color: rgb(209 213 219 / var(--tw-text-opacity));}
+"""
+
 '''main gradio function that defines all tabs'''
-with gr.Blocks(css="#row {height: 100%} .gradio-container {background-color: #E8E8DC}", title=title) as demo:
+with gr.Blocks(css=css, title=title) as demo:
     with gr.Row():
         img_funny_coy = gr.Image("./images/colored_top_image.svg")
     with gr.Tab('KG Connect & Visualization'):
@@ -22,6 +36,14 @@ with gr.Blocks(css="#row {height: 100%} .gradio-container {background-color: #E8
 
     with gr.Tab('ESG Classification'):
         esgClassificationTab.render()
-    
+
+    with gr.Row():
+        footer_links = gr.HTML("""
+                               <div class='custom-footer'>
+                                    <a href='https://www.hitec-hamburg.de/impressum/' target='_blank' rel='noreferrer' class='group hover:text-gray-400 dark:hover:text-gray-400 transition-colors'>Impressum</a>
+                                    &nbsp; • &nbsp;
+                                    <a href='https://www.hitec-hamburg.de/datenschutz/' target='_blank' rel='noreferrer' class='group hover:text-gray-400 dark:hover:text-gray-400 transition-colors'>Datenschutz</a>  
+                               </div>
+                                """)
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0")
